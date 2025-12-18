c1f83b0bd235b9ebe6b801406295b7e1d311890b
Himansu-Kumar <himansukumar7240@gmail.com>
2025-12-19 00:48:20 +0530

    add public transport structure

--- src/components/PublicTransport.jsx
+++ src/components/PublicTransport.jsx
@@ -12,6 +12,29 @@ const PublicTransport = () => {
               Need a <span className="text-g-4">Public</span> Ride ?
             </h1>
           </div>
+          <div className="flex flex-wrap justify-center gap-6">
+            {publicRoutes.map((route) => (
+              <div key={route.id} className="flex w-[40%]">
+                <div className="flex flex-col">
+                  <h1>ROUTE</h1>
+                  <div>{route.no}</div>
+                </div>
+                <div className="flex flex-col">
+                  <div className="flex">
+                    <div>{route.from}</div>
+                    <div>arrow</div>
+                    <div>{route.to}</div>
+                  </div>
+                  <div>{route.description}</div>
+                  <div>{route.frequency}</div>
+                  <div className="flex">
+                    <div>{route.fare}</div>
+                    <div>Mo bus button</div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
         <div>
           <img src={background2} alt="public" className="opacity-6 z-0" />
