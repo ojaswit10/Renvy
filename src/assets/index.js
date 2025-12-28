a4f3575f86fd3dee9f3760b82c832d996a4d7544
Himansu-Kumar <himansukumar7240@gmail.com>
2025-12-28 23:10:13 +0530

    added photos

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,18 +2,26 @@ import renvy from "./renvy.svg";
 import background from "./background.png";
 import background2 from "./background2.png";
 import bike from "./bike.png";
+import bikeOutline from "./bike-outline.png";
 import car from "./car.png";
+import carOutline from "./car-outline.png";
 import scooter from "./scooter.png";
+import scooterOutline from "./scooter-outline.png";
 import suv from "./suv.png";
-import arrow from "./arrow.svg"
+import suvOutline from "./suv-outline.png";
+import arrow from "./arrow.svg";
 
 export {
-    renvy,
-    background,
-    background2,
-    bike,
-    car,
-    scooter,
-    suv,
-    arrow
+  renvy,
+  background,
+  background2,
+  bike,
+  car,
+  scooter,
+  suv,
+  arrow,
+  bikeOutline,
+  scooterOutline,
+  carOutline,
+  suvOutline,
 };
